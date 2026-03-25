f93f47079af0bf72eb28d80699ff0986503d0641
rhit-lorenzre <lorenzre@rose-hulman.edu>
2026-03-25 12:44:22 -0400

    nav bar for every file, reduced code

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
-
+fetch('navbar.html')
+    .then(Response => {
+        if (!Response.ok) throw new Error("Navbar file not found");
+        return Response.text();
+    })
+    .then (data => document.getElementById('navbar').innerHTML = data).catch(error=> console.error(err));
